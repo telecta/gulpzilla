772cce5167f9ca911639891586c94fbdefdd34df
alvinsj <email.to.alvin@gmail.com>
2016-06-13 17:44:54 +0700

    refactor jest, sass, disc to individual packages. e.g.: gulpzilla-jest

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var invariant = require('invariant');
-var path = require('path');
 var args = require('yargs').argv,
     gulp = require('gulp'),
-    $ = require('gulp-load-plugins')(),
     opts = {args: args};
 
 module.exports = function(config){
@@ -12,44 +10,18 @@ module.exports = function(config){
 
     opts.config = config;
 
-    require('./gulp/browserify')(gulp, opts, $);
     invariant(
         config.js && typeof config.js === 'object'
             && config.js.srcDir && config.js.target
             && config.js.distDir && config.js.distFilename,
         '{srcDir, target, distDir, distFilename} in config[\'js\'] is required.');
 
-    gulp.task('default',['browserify']);
-    gulp.task('watch', ['watch-browserify']);
+    require('./gulp/browserify')(gulp, opts);
 
-    if(config.sass) {
-        invariant(
-            config.sass && typeof config.sass === 'object'
-                && config.sass.srcDir && config.sass.target
-                && config.sass.distDir && config.sass.distFilename && config.sass.loadPaths,
-            '{srcDir, target, distDir, distFilename, loadPaths} in config[\'sass\'] is required.');
-
-        require('./gulp/bower-copy')(gulp, opts, $);
-        require('./gulp/sass')(gulp, opts, $);
-
-        gulp.task('default',['sass', 'browserify']);
-        gulp.task('watch', ['watch-browserify', 'watch-sass']);
-    }
-
-    if(config.jest) {
-        require('./gulp/jest')(gulp, opts, $);
-        invariant(
-            config.jest && typeof config.jest === 'object'
-                && config.jest.srcDir,
-            '{srcDir} in config[\'jest\'] is required.');
-
-        gulp.task('test', ['jest']);
-        gulp.task('test-one', ['watch-jest']);
-    }
-
-    if(config.disc) {
-        require('./gulp/disc')(gulp, opts, $);
-    }
+    Object.keys(config).forEach((plugin) => {
+        if(['debug', 'js'].indexOf(plugin) > -1) return;
+        require('gulpzilla-'+plugin)(gulp, opts)
+    });
 
     return gulp;
 }
