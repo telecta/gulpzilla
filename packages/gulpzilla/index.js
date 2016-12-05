e3cf2c8642fd7f50792bcc4ea9d031a61fee3e05
alvinsj <code@alvinsj.net>
2016-12-05 15:30:34 +0700

    add gulpzilla standalone

---
+++ packages/gulpzilla/index.js
@@ -0,0 +1,18 @@
+var invariant = require('invariant');
+var args = require('yargs').argv,
+    gulp = require('gulp'),
+    opts = {args: args};
+
+module.exports = function(config){
+    invariant(
+        config && typeof config === 'object',
+        'gulpzilla: function argument `config` is required.');
+
+    opts.config = config;
+
+    Object.keys(config).forEach((plugin) => {
+        if(['debug'].indexOf(plugin) > -1) return;
+        require('gulpzilla-'+plugin)(gulp, opts);
+    });
+    return gulp;
+};
